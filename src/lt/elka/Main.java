01b0f78f8c253bf2a8ec668717176d5d4f21c3e0
LauryKa <laurynas.kazlauskis@gmail.com>
2017-10-08 14:50:07 +0300

    Antra versija

--- src/lt/elka/Main.java
+++ src/lt/elka/Main.java
@@ -3,6 +3,6 @@ package lt.elka;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Antra versija");
     }
 }
